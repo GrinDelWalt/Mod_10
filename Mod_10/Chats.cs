fa98568cab153cffacdf7e273ba74ac9797bc44b
Grindelwalt <Sulxj600s@gmail.com>
2022-02-23 14:41:31 +0300

    Добавьте файлы проекта.

---
+++ Mod_10/Chats.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_10
+{
+    public class Chats
+    {
+        public Chats(long id, string name, string text, DateTime date)
+        {
+            this.id = id;
+            this.name = name;
+            this.messageCollection = new List<Message>();
+            Write(text, date);
+        }
+        public void Write(string text, DateTime date)
+        {
+            messageCollection.Add(new Message(text, date));
+        }
+        public long Id { get { return this.id; } set { this.id = value; } }
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public List<Message> MessageCollection  { get { return this.messageCollection; }set { this.messageCollection = value; } }
+
+        private long id;
+        private string name;
+        private List<Message> messageCollection;
+    }
+    public class Message
+    {
+        public Message(string text, DateTime date)
+        {
+            this.text = text;
+            this.date = date;
+        }
+
+        public string Text { get { return this.text; } set { this.text = value; } }
+        public DateTime Date { get { return this.date; } set { this.date = value; } }
+
+        private string text;
+        private DateTime date;
+    }
+}
---
+++ Mod_10/ReadMessage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_10
+{
+    class ReadMessage
+    {
+        public ObservableCollection<Chats> chats;
+
+        public MainWindow window;
+
+        public Telegram.Bot.Types.Update e;
+
+        private void MessageLog()
+        {
+            chats = new ObservableCollection<Chats>();
+            Debug.WriteLine("----");
+
+            string messageText = e.Message.Text;
+            string name = e.Message.Chat.FirstName;
+            long id = e.Message.Chat.Id;
+
+            string text = $"{DateTime.Now.ToLongTimeString()}: {id} {name} {messageText}";
+
+            List<string> messages = new List<string>();
+            messages.Add(messageText);
+
+            List<DateTime> data = new List<DateTime>();
+            data.Add(DateTime.Now);
+
+            Debug.WriteLine(text);
+            
+            window.Dispatcher.Invoke(() =>
+            {
+                if (chats.Count != 0)
+                {
+                    Chats chat = chats.FirstOrDefault(x => x.Id == "id поиска");
+                    if (chat != null)
+                    {
+
+                    }
+                    else
+                    {
+                        chats.Add(new Chats(id, name, messageText, data));
+                    }
+                }
+                
+                if (chats.Id.)
+                {
+
+                }
+                
+                
+            });
+
+        }
+
+    }
+}
